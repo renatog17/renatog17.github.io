ac5a9fcb1b3e4c9179803996bc22f31e39139a87
Renato Oliveira <renatog.oliveira17@gmail.com>
2023-05-24 16:00:54 -0300

    slider images portfolio created

---
+++ portfolio/main-portfolio.js
@@ -0,0 +1,9 @@
+const list_projects = [
+    {
+        "img_src": "imgs/portfolio/todo-list.png",
+        "description": "ToDo List",
+        "icons": ["imgs/css-icon.png", "imgs/js-icon.png", "imgs/html-icon.png"]
+    }
+]
+
+console.log(list_projects)
