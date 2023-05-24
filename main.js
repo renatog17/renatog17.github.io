d6bef73e4b8eeb203a780326df855a43ac76e972
Renato Oliveira <renatog.oliveira17@gmail.com>
2023-05-24 14:26:07 -0300

    about me done

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const btnAboutMe = document.getElementById('about-me')
+const btnAboutMe = document.getElementById('btn-about-me')
 const sectionTarget = document.getElementById('s-quem-sou')
 btnAboutMe.addEventListener('click', (event)=>{
     event.preventDefault()
