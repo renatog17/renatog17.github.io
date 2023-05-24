5327d904f859e86a5232f0e68188cd22f2a7ff97
Renato Oliveira <renatog.oliveira17@gmail.com>
2023-05-24 00:29:18 -0300

    session start recriated

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
-const certificadosAlura = [
-    
-]
+const btnAboutMe = document.getElementById('about-me')
+const sectionTarget = document.getElementById('s-quem-sou')
+btnAboutMe.addEventListener('click', (event)=>{
+    event.preventDefault()
+    sectionTarget.scrollIntoView({behavior: 'smooth'})
+})
