6a829b4d8de0106069358616262d2aa9cfc8a23b
Renato Oliveira <renatog.oliveira17@gmail.com>
2023-08-05 01:01:52 -0300

    refactoring: navbar

--- main.js
+++
@@ -1,6 +0,0 @@
-const btnAboutMe = document.getElementById('btn-about-me')
-const sectionTarget = document.getElementById('s-quem-sou')
-btnAboutMe.addEventListener('click', (event)=>{
-    event.preventDefault()
-    sectionTarget.scrollIntoView({behavior: 'smooth'})
-})
--- portfolio/main-portfolio.js
+++
@@ -1,9 +0,0 @@
-const list_projects = [
-    {
-        "img_src": "imgs/portfolio/todo-list.png",
-        "description": "ToDo List",
-        "icons": ["imgs/css-icon.png", "imgs/js-icon.png", "imgs/html-icon.png"]
-    }
-]
-
-console.log(list_projects)
